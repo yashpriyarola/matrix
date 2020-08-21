5e615d7557eaddb8c9021fd6a9ef1ec112c50f22
yashpriyarola <69459432+yashpriyarola@users.noreply.github.com>
2020-08-21 17:24:47 +0530

    Update matrix_SumDiagonal.cpp

--- matrix_SumDiagonal.cpp
+++ matrix_SumDiagonal.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int m,n,y,x,*p[10][10],q;
+    int m,n,y,x,*p[10][10];
     printf("enter the no of rows and column for matrix:",m ,n);
-    scanf("%d%d",&m ,&n);
+    scanf("%d%d",&m ,&n);//rows and columns
     int a[10][10],i,j;
     for(i=0;i<m;i++)
     {
       for(j=0;j<n;j++)  
         {
            printf("enter the values for matrix element[%d][%d]:",i, j);
-           scanf("%d",&a[i][j]);
+           scanf("%d",&a[i][j]);//getting values
            p[i][j] = &a[i][j];
             }
         }
@@ -20,7 +20,7 @@ int main()
       for(j=0;j<n;j++)  
         { 
             
-           printf("%d\t",*p[i][j]);
+           printf("%d\t",*p[i][j]);//print diagonal
            
        }
        printf("\n");
@@ -32,11 +32,11 @@ int main()
             {
          if(i==j) 
           {
-               y=y+a[i][j];
+               y=y+a[i][j];//main diagonal
            }
         if (i+j==2)
             {
-               x=x+a[i][j];
+               x=x+a[i][j];//secondary diagonal
           }}}
        
       printf("sum of the main/ principal diagonal is %d\n",y);
